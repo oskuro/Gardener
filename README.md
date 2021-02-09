77b84403fdfbe0a7f7ca9047f1600bbfdc423bd0
Lukas Isaksson <lukas.isaksson@gmail.com>
2021-02-09 07:48:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gardener
+
+Developed with Unreal Engine 4
---
+++ Source/Gardener/DA_Crop.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DA_Crop.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GARDENER_API UDA_Crop : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
